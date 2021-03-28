6403cbc48698760c1b0b1b440107143d79293662
CoffeeCoding <msdevacc@live.de>
2021-03-28 15:38:33 +0200

    Added Help Page

--- lib/main.dart
+++ lib/main.dart
@@ -166,7 +166,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
       case DisplayedPage.help:
         {
           _title = 'Help Page';
-          _body = buildHomePage(context);
+          _body = buildHelpPage(context);
           break;
         }
       case DisplayedPage.about:
@@ -186,7 +186,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
         child: Scaffold(
             backgroundColor: Theme.of(context).backgroundColor,
             appBar: AppBar(
-              title: Text(_title, style: Theme.of(context).textTheme.bodyText2),
+              title: Text(_title, style: Theme.of(context).textTheme.headline5),
             ),
             drawer: Drawer(
               elevation: 5.0,
@@ -785,6 +785,23 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
                   style: Theme.of(context).textTheme.headline2);
             }
           }),
+    );
+  }
+
+  Widget buildHelpPage(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: Text('How to use', style: Theme.of(context).textTheme.bodyText1,
+                textAlign: TextAlign.start),
+          ),
+          Text('This is an elaborate description in extreme detail of what this application is supposed to be used for and how exactly one goes about using it. Basically it is an app that should give the user a visual impression of where their time goes each day. This may help the user rationalise their time usage and cut out time-wasters from their daily routines. Needless to say, the benefit is only as good as the effort put up into using this app properly. Luckily, the app is designed to be as easy to use as possible and require as little time as possible. So let\'s take a look at how to use it using an example.',
+              style: Theme.of(context).textTheme.bodyText2),
+        ]
+      ),
     );
   }
 }
